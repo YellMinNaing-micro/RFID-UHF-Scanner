94007ece763a5049a4f956c5f54c42badb9d73e1
Yell Min Naing <yellminnaing5@gmail.com>
2025-09-01 16:46:31 +0630

    Create UhfPackage.kt

---
+++ android/app/src/main/java/com/mobileapp/uhf/UhfPackage.kt
@@ -0,0 +1,12 @@
+package com.yourapp.uhf
+import com.facebook.react.ReactPackage
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.uimanager.ViewManager
+import android.app.Application
+
+class UhfPackage : ReactPackage {
+  override fun createNativeModules(reactContext: com.facebook.react.bridge.ReactApplicationContext)
+    : List<NativeModule> = listOf(UhfModule(reactContext))
+  override fun createViewManagers(reactContext: com.facebook.react.bridge.ReactApplicationContext)
+    : List<ViewManager<*, *>> = emptyList()
+}
