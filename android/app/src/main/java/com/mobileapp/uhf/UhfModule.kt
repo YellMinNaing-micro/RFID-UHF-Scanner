d59345b5c744a3cc575b22244790b22bdd52afaa
Yell Min Naing <yellminnaing5@gmail.com>
2025-09-02 13:41:03 +0630

    Update UhfModule.kt

--- android/app/src/main/java/com/mobileapp/uhf/UhfModule.kt
+++ android/app/src/main/java/com/mobileapp/uhf/UhfModule.kt
@@ -4,63 +4,47 @@ import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
 import com.facebook.react.bridge.Promise
-import com.handheld.uhfr.Reader
+import com.uhf.api.cls.Reader
 
 class UhfModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
 
-    // Correctly declare the reader using the 'Reader' class
     private var reader: Reader? = null
 
     override fun getName(): String = "UhfModule"
 
     init {
         try {
-            // Use the correct static initialization method.
-            // This is a common pattern for these SDKs.
-            reader = Reader.get
+            reader = Reader()
+            reader?.InitReader_Notype("", Reader.MAXANTCNT)
         } catch (e: Exception) {
             e.printStackTrace()
         }
     }
 
     @ReactMethod
-    fun readTag(promise: Promise) {
+    fun readAllTags(promise: Promise) {
         try {
-            // Use the correct method to perform an inventory.
-            // This typically returns a list of tags. We'll grab the first one.
-            val inventoryData = reader?.inventorySingleTag()
-            if (inventoryData != null && inventoryData.isNotEmpty()) {
-                val tagId = inventoryData[0].epc.toString()
-                promise.resolve(tagId)
-            } else {
-                promise.reject("NO_TAG", "No UHF tag found")
-            }
-        } catch (e: Exception) {
-            promise.reject("READ_ERROR", e.message, e)
-        }
-    }
+            val antCounts = IntArray(Reader.MAXANTCNT)
+            val tagCount = IntArray(1)
 
-    @ReactMethod
-    fun writeTag(data: String, promise: Promise) {
-        // Method name is a guess, you may need to consult the documentation
-        // for the correct method signature and parameters.
-        try {
-            val success = reader?.writeTag(data) ?: false
-            if (success) {
-                promise.resolve("WRITE_SUCCESS")
+            val tagInfos: Array<Reader.TAGINFO?> = Array(Reader.MAXINVPOTLSCNT) { null }
+
+            val result: Int? = reader?.TagInventory(antCounts, antCounts.size, 1, tagInfos, tagCount)
+
+            if (result == 0) { // 0 = success
+                promise.resolve(arrayOf<String>()) // empty array for now
             } else {
-                promise.reject("WRITE_FAIL", "Failed to write tag")
+                promise.reject("READ_ERROR", "Reader error: $result")
             }
         } catch (e: Exception) {
-            promise.reject("WRITE_ERROR", e.message, e)
+            promise.reject("READ_EXCEPTION", e.message, e)
         }
     }
 
     override fun onCatalystInstanceDestroy() {
         super.onCatalystInstanceDestroy()
         try {
-            // The correct method to close the connection.
-            reader?.close()
+            reader?.CloseReader()
         } catch (e: Exception) {
             e.printStackTrace()
         }
