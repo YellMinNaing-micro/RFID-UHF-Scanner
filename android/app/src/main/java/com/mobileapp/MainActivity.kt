5992bee27efbf925873eb47acd23d79f7d337110
Yell Min Naing <yellminnaing5@gmail.com>
2025-09-10 13:55:51 +0630

    Update MainActivity.kt

--- android/app/src/main/java/com/mobileapp/MainActivity.kt
+++ android/app/src/main/java/com/mobileapp/MainActivity.kt
@@ -6,8 +6,6 @@ import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
-import com.facebook.react.bridge.Arguments
-import com.facebook.react.modules.core.DeviceEventManagerModule
 
 class MainActivity : ReactActivity() {
 
@@ -19,11 +17,7 @@ class MainActivity : ReactActivity() {
     override fun dispatchKeyEvent(event: KeyEvent): Boolean {
         if (event.action == KeyEvent.ACTION_DOWN && event.keyCode == KeyEvent.KEYCODE_F7) {
             Log.d("MainActivity", "Hardware trigger pressed (F7)")
-            val params = Arguments.createMap()
-            params.putString("key", "F7")
-            reactInstanceManager.currentReactContext
-                ?.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter::class.java)
-                ?.emit("onHardwareTrigger", params)
+            // The UhfModule broadcast receiver will handle the scan trigger
             return true
         }
         return super.dispatchKeyEvent(event)
