822a61e4c012e7d2d320b133066dad29f814e982
Yell Min Naing <yellminnaing5@gmail.com>
2025-09-10 12:01:40 +0630

    Update MainActivity.kt

--- android/app/src/main/java/com/mobileapp/MainActivity.kt
+++ android/app/src/main/java/com/mobileapp/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.mobileapp
 
+import android.view.KeyEvent
+import android.util.Log
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+import com.facebook.react.bridge.Arguments
+import com.facebook.react.modules.core.DeviceEventManagerModule
 
 class MainActivity : ReactActivity() {
 
@@ -11,4 +15,17 @@ class MainActivity : ReactActivity() {
 
     override fun createReactActivityDelegate(): ReactActivityDelegate =
         DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+
+    override fun dispatchKeyEvent(event: KeyEvent): Boolean {
+        if (event.action == KeyEvent.ACTION_DOWN && event.keyCode == KeyEvent.KEYCODE_F7) {
+            Log.d("MainActivity", "Hardware trigger pressed (F7)")
+            val params = Arguments.createMap()
+            params.putString("key", "F7")
+            reactInstanceManager.currentReactContext
+                ?.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter::class.java)
+                ?.emit("onHardwareTrigger", params)
+            return true
+        }
+        return super.dispatchKeyEvent(event)
+    }
 }
