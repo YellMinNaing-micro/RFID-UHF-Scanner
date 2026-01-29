6bb1735411d5b8938f551b116b65f60d32a5e890
YellMinNaing-micro <yellminnaing5@gmail.com>
2026-01-29 15:46:18 +0630

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from './screens/Login';
-import RegisterScreen from './screens/Register';
 import HomeScreen from "./screens/Home";
 import UhfScannerScreen from "./screens/UhfScanner";
 import DetailScreen from "./screens/Detail";
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 export type RootStackParamList = {
     Login: undefined;
@@ -19,15 +19,19 @@ const Stack = createStackNavigator<RootStackParamList>();
 
 const App = () => {
     return (
+      <SafeAreaProvider>
         <NavigationContainer>
-            <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
-                <Stack.Screen name="Login" component={LoginScreen} />
-                {/*<Stack.Screen name="Register" component={RegisterScreen} />*/}
-                <Stack.Screen name="Home" component={HomeScreen} />
-                <Stack.Screen name="UhfScanner" component={UhfScannerScreen} />
-                <Stack.Screen name="Detail" component={DetailScreen} />
-            </Stack.Navigator>
+          <Stack.Navigator
+            initialRouteName="Login"
+            screenOptions={{headerShown: false}}>
+            <Stack.Screen name="Login" component={LoginScreen} />
+            {/*<Stack.Screen name="Register" component={RegisterScreen} />*/}
+            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="UhfScanner" component={UhfScannerScreen} />
+            <Stack.Screen name="Detail" component={DetailScreen} />
+          </Stack.Navigator>
         </NavigationContainer>
+      </SafeAreaProvider>
     );
 };
 
